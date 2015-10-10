2e19cd279e6f27c5ab04144de7ecc280e8de8073
Praful Shankar <praful.shankare@berkeley.edu>
2015-10-10 07:26:33 -0700

    added bin and deleted randos

--- setup.py
+++ setup.py
@@ -7,4 +7,8 @@ setup(
     packages = ['pizzasearch',],
     license='Creative Commons Attribution-Noncommercial-Share Alike license',
     long_description = open('README.txt').read(),
+    author = "Praful Shankar",
+    author_email = "praful.shankar@berkeley.edu",
+    url = "https://github.com/prafulshankar/pizzasearch",
+    download_url="https://github.com/prafulshankar/pizzasearch/tarball/0.1dev",
 )
