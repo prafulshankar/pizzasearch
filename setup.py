79f74ee4352faef42c4b00eb8103f6f074c1f425
Praful Shankar <praful.shankare@berkeley.edu>
2015-10-10 06:27:56 -0700

    inital setup and README.txt

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+
+setup(
+    name = "pizzasearch",
+    version = "0.1dev",
+    packages = ['pizzasearch',],
+    license='Creative Commons Attribution-Noncommercial-Share Alike license',
+    long_description = open('README.txt').read(),
+)
