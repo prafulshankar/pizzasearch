c4286a26895b585ed82daa64bbe2e82b7ffab1d7
Praful Shankar <praful.shankare@berkeley.edu>
2015-10-10 07:36:16 -0700

    fixed dependency issues

--- setup.py
+++ setup.py
@@ -11,4 +11,8 @@ setup(
     author_email = "praful.shankar@berkeley.edu",
     url = "https://github.com/prafulshankar/pizzasearch",
     download_url="https://github.com/prafulshankar/pizzasearch/tarball/0.1dev",
+    install_requires = ['piazza-api >= 0.5.1'],
+    dependency_links = [
+        "https://pypi.python.org/pypi/piazza-api"
+    ],
 )
