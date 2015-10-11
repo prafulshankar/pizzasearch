b17467ff47ec5365b3f89590e5ee71ff91ab39de
Jack Thakar <jathak@gmail.com>
2015-10-11 01:31:02 -0700

    fix typo

--- pizza/utils.py
+++ pizza/utils.py
@@ -3,8 +3,8 @@ def filter_plus_ones(followup):
     plus_ones = 0
     if 'plus_ones' in followup:
         plus_ones = followup['plus_ones']
-    for child in followup['children']:\
-        sub = child['subject'].replace(' ', '')
+    for child in followup['children']:
+        sub = child['subject'].replace(" ", "")
         if sub == '+1' or sub == '<p>+1</p>':
             plus_ones += 1
         else:
