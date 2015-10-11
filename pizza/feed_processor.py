75bfe8fad4b64f0c8d8240205bf9c0a57732f698
Jack Thakar <jathak@gmail.com>
2015-10-10 19:24:56 -0700

    adds feed_processor

---
+++ pizza/feed_processor.py
@@ -0,0 +1,77 @@
+from piazza_api2 import Piazza
+from piazza_api2.network import FollowingFilter, FolderFilter
+
+class FeedProcessor:
+    
+    def __init__(self, network, query): 
+        self.network = network
+        self.query = query
+        self.posts = []
+        self.feed = None
+        self.feed_index = -1
+        self.offset = 0
+        self.complete = False
+    
+    def __getitem__(self, arg):
+        while arg >= len(self.posts):
+            self.get_post()
+            
+        return self.posts[arg]
+        
+    def next_post(self):
+        if not self.feed or self.feed_index >= len(self.feed):
+            if self.complete:
+                return None
+            else:
+                self.load_feed()
+            
+        while True:
+            if not self.feed:
+                return None
+            if self.feed_index == -1 or self.feed_index >= len(self.feed):
+                return None
+            post = self.feed[self.feed_index]
+            self.feed_index += 1
+            if self.post_allowed(post):
+                return post
+    
+    def post_allowed(self, post):
+        if self.query.tag and self.query.tag not in post['folders']:
+            return False
+        if self.query.pinned and 'pin' not in post['tags']:
+            return False
+        if self.query.following and ('book' not in post or post['book'] == 0):
+            return False
+        if self.query.inst_notes and 'instructor-note' not in post['tags'] and \
+                'instructor-question' not in post['tags'] and \
+                'instructor-poll' not in post['tags']:
+            return False
+        return True
+    
+    def load_feed(self):
+        self.feed_index = 0
+        if self.query.query:
+            if not self.feed:
+                self.feed =  self.network.search_feed(self.query.query)
+            self.complete = True
+        elif self.query.following:
+            if not self.feed:
+                self.feed = self.network.get_filtered_feed(FollowingFilter())['feed']
+            self.complete = True
+        elif self.query.tag:
+            if not self.feed:
+                self.feed = self.network.get_filtered_feed(FolderFilter(self.query.tag))['feed']
+            self.complete = True
+        elif not self.feed:
+            self.feed = self.network.get_feed()['feed']
+        elif len(self.feed) >= 100:
+            self.offset += len(self.feed)
+            self.feed = self.network.get_feed(offset=self.offset)['feed']
+        else:
+            self.feed = None
+            self.complete = True
+            
+        return self.feed
+    
+    
+    
--- pizza/pizza.py
+++ pizza/pizza.py
@@ -6,6 +6,8 @@ import pickle
 import os 
 import getpass
 
+from feed_processor import FeedProcessor
+
 class InputError(Exception): 
 
     def __init__(self, msg): 
@@ -17,23 +19,28 @@ class InputError(Exception):
 class QueryObj: 
 
     def add_query(self, query):
-        self.query = ''.join(query)
+        if query is None:
+            self.query = None
+        else:
+            self.query = ''.join(query)
 
-    def add_tags(self, list_of_tags):
-        if (list_of_tags is not None): 
-            self.tags = list_of_tags
+    def add_tag(self, tag):
+        self.tag = tag
 
     def add_time_range(self, range_list): 
         if (range_list is not None):
             self.tr_range = range(int(range_list[0]), int(range_list[1]))
+        else:
+            self.tr_range = None
 
     def bool_pinned(self, pinned): 
-        if (pinned is not None):
-            self.pinned = pinned 
+        self.pinned = pinned 
 
     def bool_inst_notes(self, inst): 
-        if (inst is not None): 
-            self.inst_notes = inst
+        self.inst_notes = inst
+            
+    def bool_following(self, follow):
+        self.following = follow
 
     def __repr__(self): 
         return "Query: " + self.query + "\n List_of_tags: " + str(self.tags) + "\n Time Range " + repr(self.tr_range) + "\n Pinned: " + str(self.pinned) + "\n Inst notes: " + str(self.inst_notes)
@@ -46,22 +53,21 @@ def main():
 
     parser = argparse.ArgumentParser(description='Process user input for piazza queries')
     parser.add_argument('-q', '--query', nargs="+")
-    parser.add_argument('-t', '--tags', nargs="+") 
+    parser.add_argument('-t', '--tag', nargs=1) 
     parser.add_argument('-r', '--range', nargs=2)
     parser.add_argument('-i', '--instructor-only', action='store_true')
-    parser.add_argument('-p', '--posts', action='store_true')
+    parser.add_argument('-p', '--pinned', action='store_true')
+    parser.add_argument('-f', '--following', action='store_true')
     parser.add_argument('-l', '--force-login', action='store_true')
     args = parser.parse_args()
 
-    if (args.query is None): 
-        raise(InputError("Query not given!"))
-
     queryObj = QueryObj()
     queryObj.add_query(args.query)
-    queryObj.add_tags(args.tags)
+    queryObj.add_tag(args.tag)
     queryObj.add_time_range(args.range)
     queryObj.bool_inst_notes(args.instructor_only)
-    queryObj.bool_pinned(args.posts)
+    queryObj.bool_pinned(args.pinned)
+    queryObj.bool_following(args.following)
 
     loginfile = os.path.expanduser("~") + "/.pizza"
     
@@ -91,6 +97,17 @@ def main():
             info = '(inactive) ' + info
         print '{0:2d}: {1:s}'.format(counter, info)
         counter = counter + 1
+        
+    index = raw_input('Class Number: ')
+    network = piazza.network(classes[int(index) - 1]['id'])
+    feed_processor = FeedProcessor(network, queryObj)
+    
+    for i in range(0, 5):
+        post = feed_processor.next_post()
+        if post is None:
+            print "No post."
+        else:
+            print post['subject']
 
 
 if __name__ == '__main__':
